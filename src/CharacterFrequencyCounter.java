f938d47f7fb7f1bfcf4cc31e044f45331271e2ce
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-01-16 22:08:08 -0800

    completed junit tests

--- src/CharacterFrequencyCounter.java
+++ src/CharacterFrequencyCounter.java
@@ -49,6 +49,7 @@ public class CharacterFrequencyCounter {
     public int getFrequency(char c) {
         c = Character.toLowerCase(c);  // Case insensitive lookup
         return frequencyMap.getOrDefault(c, 0);
+    
     }
 
     /**
@@ -60,7 +61,13 @@ public class CharacterFrequencyCounter {
      */
     public double getRelativePercentage(char c) {
         c = Character.toLowerCase(c);
-        int charCount = frequencyMap.get(c);
+        int charCount =0;
+        if(frequencyMap.get(c) !=null)
+        {
+            charCount=frequencyMap.get(c);
+    
+        }   
+    
 
         return ((double) charCount / totalCharacterCount) * 100;
     }
--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -2,6 +2,36 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class CharacterFrequencyCounterTest {
+public class CharacterFrequencyCounterTest 
+{
+
+  @Test
+  public void textLAppearsThreeTimesinHelloWorld()
+  {
+    // Arrange
+    CharacterFrequencyCounter counter =new CharacterFrequencyCounter("Hello World");
+
+    // Act
+    int actualCount = counter.getFrequency('l');
     
+    // Assert
+    assertEquals(3, actualCount);
+
+  }
+
+
+  @Test
+  public void testNonOccuringCharactersPerentage()
+  {
+    // Arrange
+    CharacterFrequencyCounter counter =new CharacterFrequencyCounter("hi there");
+
+    // Act
+  double actualPercentaget = counter.getRelativePercentage('z');
+    
+    // Assert
+    assertEquals(0.0, actualPercentaget, 0.0001);
+
+  }
+
 }
